fae4e6e17880c84dd06e648623999a67806a8545
skrishkaran-ship-it <skrishkaran@gmail.com>
2025-12-15 04:27:27 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,7 +6,7 @@ import pandas as pd
 # -----------------------------
 @st.cache_data
 def load_data():
-    return pd.read_csv("earthquakes.csv")
+    return pd.read_csv("streamlit/earthquakes.csv")
 
 # -----------------------------
 # Page Configuration
